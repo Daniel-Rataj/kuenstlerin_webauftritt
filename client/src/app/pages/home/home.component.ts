aedd9cc6b226099f867f08637c4682fc27168826
Thomas <tb10091>
2025-05-08 20:13:09 +0200

    weiterleitung

---
+++ client/src/app/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,  // Falls du Standalone-Komponenten verwendest
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  // Deine Logik für die Home-Komponente
+}
