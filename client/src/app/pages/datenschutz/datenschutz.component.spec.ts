4d38c0b4bc3161cb68ad6adba91babab270c4ebc
mg11639 <marko.granula@stud.th-deg.de>
2025-06-25 19:22:40 +0200

    datenschutzseite erstellt, #1. impressumseite erstellt, #2.

--- client/src/app/pages/datenschutz/datenschutz.component.spec.ts
+++ client/src/app/pages/datenschutz/datenschutz.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FooterComponent } from './footer.component';
+import { DatenschutzComponent } from './datenschutz.component';
 
-describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+describe('DatenschutzComponent', () => {
+  let component: DatenschutzComponent;
+  let fixture: ComponentFixture<DatenschutzComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FooterComponent]
+      imports: [DatenschutzComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(FooterComponent);
+    fixture = TestBed.createComponent(DatenschutzComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
