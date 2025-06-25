cfba33d7425c11ed3143d90a9b847c561613b17a
Daniel-Rataj <danielrataj@web.de>
2025-06-25 20:20:33 +0200

    Build Fehler behoben

--- client/src/app/layout/dashboard/moderator/gallery-post/exhibition-details/exhibition-details.component.ts
+++ client/src/app/layout/dashboard/moderator/gallery-post/exhibition-details/exhibition-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ExhibitionDto } from '../../../../../models/dto/exhibition.dto';
 import { ActivatedRoute } from '@angular/router';
 import { ExhibitionService } from '../../../../../services/exhibition/exhibition.service';
