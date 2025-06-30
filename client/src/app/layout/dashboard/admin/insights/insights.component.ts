6a98a2ff4f37f2fbad20b7f5ba60c2ea798456e2
Daniel-Rataj <danielrataj@web.de>
2025-06-30 14:20:51 +0200

    DashboardHeader und Toolbar zu Insights hinzugefügt

--- client/src/app/layout/dashboard/admin/insights/insights.component.ts
+++ client/src/app/layout/dashboard/admin/insights/insights.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { DashboardHeaderComponent } from '../../shared/dashboard-header/dashboard-header.component';
+import { DashboardToolbarComponent } from '../../shared/dashboard-toolbar/dashboard-toolbar.component';
 
 @Component({
   selector: 'app-insights',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, DashboardHeaderComponent, DashboardToolbarComponent],
   templateUrl: './insights.component.html',
   styleUrl: './insights.component.scss'
 })
--- client/src/app/layout/dashboard/admin/user-management/user-management.component.ts
+++ client/src/app/layout/dashboard/admin/user-management/user-management.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserTableComponent } from './user-table/user-table.component';
+import { DashboardHeaderComponent } from '../../shared/dashboard-header/dashboard-header.component';
+import { DashboardToolbarComponent } from '../../shared/dashboard-toolbar/dashboard-toolbar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-management',
   standalone: true,
-  imports: [CommonModule, UserTableComponent],
+  imports: [CommonModule, RouterModule, UserTableComponent, DashboardHeaderComponent, DashboardToolbarComponent],
   templateUrl: './user-management.component.html',
   styleUrl: './user-management.component.scss'
 })
